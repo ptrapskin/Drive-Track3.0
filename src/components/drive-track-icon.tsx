529bf1cfad53567a958f7283181058190db68ca6
Philip Trapskin <hello@drive-track.com>
2025-08-07 13:16:15 +0000

    logo is not updating with new png file

--- src/components/drive-track-icon.tsx
+++ src/components/drive-track-icon.tsx
@@ -5,8 +5,8 @@ import Image from 'next/image';
 
 const DriveTrackIcon = ({ className }: { className?: string }) => (
   <Image
-    src="/images/drive-track-logo.png"
-    alt="Drive-Track Logo"
+    src={`/images/drive-track-logo.png?v=${new Date().getTime()}`}
+    alt="Drive-Track Logo - Steering Wheel"
     width={32}
     height={32}
     className={className}
